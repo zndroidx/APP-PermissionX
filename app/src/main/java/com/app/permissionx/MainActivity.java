ba3a594480bfdc0397967e92e5c1cbea0678547e
lazy <luzhenyuxfcy@sina.com>
2020-08-02 22:40:36 +0800

    add code

--- app/src/main/java/com/app/permissionx/MainActivity.java
+++ app/src/main/java/com/app/permissionx/MainActivity.java
@@ -1,8 +1,22 @@
 package com.app.permissionx;
 
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
+import com.zndroid.permission.PermissionX;
+import com.zndroid.permission.callback.ExplainReasonCallbackWithBeforeParam;
+import com.zndroid.permission.callback.ForwardToSettingsCallback;
+import com.zndroid.permission.callback.RequestCallback;
+import com.zndroid.permission.request.ExplainScope;
+import com.zndroid.permission.request.ForwardScope;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +24,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button makeCallBtn = findViewById(R.id.makeCallBtn);
+        makeCallBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PermissionX.init(MainActivity.this)
+                        .permissions(
+                                Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .onExplainRequestReason(new ExplainReasonCallbackWithBeforeParam() {
+                            @Override
+                            public void onExplainReason(ExplainScope scope, List<String> deniedList, boolean beforeRequest) {
+                                scope.showRequestReasonDialog(deniedList, "为了保证程序正常工作，请您同意以下权限申请" + deniedList, "我已明白");
+                            }
+                        })
+                        .onForwardToSettings(new ForwardToSettingsCallback() {
+                            @Override
+                            public void onForwardToSettings(ForwardScope scope, List<String> deniedList) {
+                                scope.showForwardToSettingsDialog(deniedList, "您需要去应用程序设置当中手动开启权限" + deniedList, "我已明白");
+                            }
+                        })
+                        .request(new RequestCallback() {
+                            @Override
+                            public void onResult(boolean allGranted, List<String> grantedList, List<String> deniedList) {
+                                if (allGranted) {
+                                    Toast.makeText(MainActivity.this, "所有申请的权限都已通过", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "您拒绝了如下权限：" + deniedList, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
---
+++ lib_permissionX/src/main/java/com/zndroid/permission/callback/ExplainReasonCallbackWithBeforeParam.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C)  guolin, PermissionX Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.zndroid.permission.callback;
+
+import com.zndroid.permission.request.ExplainScope;
+
+import java.util.List;
+
+/**
+ * Callback for {@link com.zndroid.permission.request.PermissionBuilder#onExplainRequestReason(ExplainReasonCallbackWithBeforeParam)} method.
+ *
+ * @author guolin
+ * @since 2020/6/7
+ */
+public interface ExplainReasonCallbackWithBeforeParam {
+
+    /**
+     * Called when you should explain why you need these permissions.
+     * @param scope
+     *          Scope to show rationale dialog.
+     * @param deniedList
+     *          Permissions that you should explain.
+     * @param beforeRequest
+     *          Indicate it's before or after permission request. Work with {@link com.zndroid.permission.request.PermissionBuilder#explainReasonBeforeRequest()}
+     */
+    void onExplainReason(ExplainScope scope, List<String> deniedList, boolean beforeRequest);
+
+}
