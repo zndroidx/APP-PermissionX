3279cbede999daa82c9459917e91d15103b94aeb
lazy <luzhenyuxfcy@sina.com>
2020-08-06 00:16:49 +0800

    优化

--- app/src/main/java/com/app/permissionx/CustomDialog.java
+++ app/src/main/java/com/app/permissionx/CustomDialog.java
@@ -2,6 +2,9 @@ package com.app.permissionx;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PermissionGroupInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -42,51 +45,76 @@ public class CustomDialog extends RationaleDialog {
     private Button positiveBtn;
     private LinearLayout permissionsLayout;
 
+    private boolean showDescription;
+
     private WeakReference<Context> context;
 
     public CustomDialog(@NonNull Context context, String message, List<String> permissions) {
-        super(context);
+        this(context, message, permissions, false);
+    }
+
+    public CustomDialog(@NonNull Context context, String message, List<String> permissions, boolean showDescription) {
+        super(context, R.style.CustomDialog);
         this.message = message;
         this.permissions = permissions;
         this.context = new WeakReference<>(context);
         this.groupSet = new HashSet<>();
+        this.showDescription = showDescription;
+
+        initPermissionPools();
     }
 
     private void initPermissionPools(){
-        permissionMap.put(Manifest.permission_group.CALENDAR, Manifest.permission.READ_CALENDAR);
-        permissionMap.put(Manifest.permission_group.CALENDAR, Manifest.permission.WRITE_CALENDAR);
-        permissionMap.put(Manifest.permission_group.CALL_LOG, Manifest.permission.READ_CALL_LOG);
-        permissionMap.put(Manifest.permission_group.CALL_LOG, Manifest.permission.WRITE_CALL_LOG);
-        permissionMap.put(Manifest.permission_group.CALL_LOG, Manifest.permission.PROCESS_OUTGOING_CALLS);
-        permissionMap.put(Manifest.permission_group.CAMERA, Manifest.permission.CAMERA);
-        permissionMap.put(Manifest.permission_group.CONTACTS, Manifest.permission.READ_CONTACTS);
-        permissionMap.put(Manifest.permission_group.CONTACTS, Manifest.permission.WRITE_CONTACTS);
-        permissionMap.put(Manifest.permission_group.CONTACTS, Manifest.permission.GET_ACCOUNTS);
-        permissionMap.put(Manifest.permission_group.LOCATION, Manifest.permission.ACCESS_FINE_LOCATION);
-        permissionMap.put(Manifest.permission_group.LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION);
-        permissionMap.put(Manifest.permission_group.LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION);
-        permissionMap.put(Manifest.permission_group.MICROPHONE, Manifest.permission.RECORD_AUDIO);
-        permissionMap.put(Manifest.permission_group.PHONE, Manifest.permission.READ_PHONE_STATE);
-        permissionMap.put(Manifest.permission_group.PHONE, Manifest.permission.READ_PHONE_NUMBERS);
-        permissionMap.put(Manifest.permission_group.PHONE, Manifest.permission.CALL_PHONE);
-        permissionMap.put(Manifest.permission_group.PHONE, Manifest.permission.ANSWER_PHONE_CALLS);
-        permissionMap.put(Manifest.permission_group.PHONE, Manifest.permission.ADD_VOICEMAIL);
-        permissionMap.put(Manifest.permission_group.PHONE, Manifest.permission.USE_SIP);
-        permissionMap.put(Manifest.permission_group.PHONE, Manifest.permission.ACCEPT_HANDOVER);
-        permissionMap.put(Manifest.permission_group.SENSORS, Manifest.permission.BODY_SENSORS);
-        permissionMap.put(Manifest.permission_group.ACTIVITY_RECOGNITION, Manifest.permission.ACTIVITY_RECOGNITION);
-        permissionMap.put(Manifest.permission_group.SMS, Manifest.permission.SEND_SMS);
-        permissionMap.put(Manifest.permission_group.SMS, Manifest.permission.RECEIVE_SMS);
-        permissionMap.put(Manifest.permission_group.SMS, Manifest.permission.READ_SMS);
-        permissionMap.put(Manifest.permission_group.SMS, Manifest.permission.RECEIVE_WAP_PUSH);
-        permissionMap.put(Manifest.permission_group.SMS, Manifest.permission.RECEIVE_MMS);
-        permissionMap.put(Manifest.permission_group.STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE);
-        permissionMap.put(Manifest.permission_group.STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        permissionMap.put(Manifest.permission_group.STORAGE, Manifest.permission.ACCESS_MEDIA_LOCATION);
-    }
-
-    public CustomDialog(@NonNull Context context) {
-        super(context, R.style.CustomDialog);
+        permissionMap.put(Manifest.permission.READ_CALENDAR, Manifest.permission_group.CALENDAR);
+        permissionMap.put(Manifest.permission.WRITE_CALENDAR, Manifest.permission_group.CALENDAR);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            permissionMap.put(Manifest.permission.READ_CALL_LOG, Manifest.permission_group.CALL_LOG);
+            permissionMap.put(Manifest.permission.WRITE_CALL_LOG, Manifest.permission_group.CALL_LOG);
+            permissionMap.put(Manifest.permission.PROCESS_OUTGOING_CALLS, Manifest.permission_group.CALL_LOG);
+        }
+        permissionMap.put(Manifest.permission.CAMERA, Manifest.permission_group.CAMERA);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            permissionMap.put(Manifest.permission.READ_CONTACTS, Manifest.permission_group.CONTACTS);
+            permissionMap.put(Manifest.permission.WRITE_CONTACTS, Manifest.permission_group.CONTACTS);
+            permissionMap.put(Manifest.permission.GET_ACCOUNTS, Manifest.permission_group.CONTACTS);
+        }
+        permissionMap.put(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission_group.LOCATION);
+        permissionMap.put(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission_group.LOCATION);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            permissionMap.put(Manifest.permission.ACCESS_BACKGROUND_LOCATION, Manifest.permission_group.LOCATION);
+        }
+        permissionMap.put(Manifest.permission.RECORD_AUDIO, Manifest.permission_group.MICROPHONE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            permissionMap.put(Manifest.permission.READ_PHONE_STATE, Manifest.permission_group.PHONE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                permissionMap.put(Manifest.permission.READ_PHONE_NUMBERS, Manifest.permission_group.PHONE);
+                permissionMap.put(Manifest.permission.ANSWER_PHONE_CALLS, Manifest.permission_group.PHONE);
+            }
+            permissionMap.put(Manifest.permission.CALL_PHONE, Manifest.permission_group.PHONE);
+            permissionMap.put(Manifest.permission.ADD_VOICEMAIL, Manifest.permission_group.PHONE);
+            permissionMap.put(Manifest.permission.USE_SIP, Manifest.permission_group.PHONE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                permissionMap.put(Manifest.permission.ACCEPT_HANDOVER, Manifest.permission_group.PHONE);
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            permissionMap.put(Manifest.permission.BODY_SENSORS, Manifest.permission_group.SENSORS);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            permissionMap.put(Manifest.permission.ACTIVITY_RECOGNITION, Manifest.permission_group.ACTIVITY_RECOGNITION);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            permissionMap.put(Manifest.permission.SEND_SMS, Manifest.permission_group.SMS);
+            permissionMap.put(Manifest.permission.RECEIVE_SMS, Manifest.permission_group.SMS);
+            permissionMap.put(Manifest.permission.READ_SMS, Manifest.permission_group.SMS);
+            permissionMap.put(Manifest.permission.RECEIVE_WAP_PUSH, Manifest.permission_group.SMS);
+            permissionMap.put(Manifest.permission.RECEIVE_MMS, Manifest.permission_group.SMS);
+        }
+        permissionMap.put(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission_group.STORAGE);
+        permissionMap.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission_group.STORAGE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            permissionMap.put(Manifest.permission.ACCESS_MEDIA_LOCATION, Manifest.permission_group.STORAGE);
+        }
     }
 
     @Override
@@ -132,10 +160,16 @@ public class CustomDialog extends RationaleDialog {
             Iterator iterator = entries.iterator();
             while (iterator.hasNext()) {
                 Map.Entry<String, String> entry = (Map.Entry<String, String>) iterator.next();
-                if (entry.getValue().equals(p)) {
-                    String permissionGroup = entry.getKey();
+                if (entry.getKey().equals(p)) {
+                    String permissionGroup = entry.getValue();
                     if (permissionGroup != null && !groupSet.contains(permissionGroup)) {
                         TextView textView = (TextView) LayoutInflater.from(context.get()).inflate(R.layout.permissions_item, permissionsLayout, false);
+                        try {
+                            PermissionGroupInfo groupInfo = context.get().getPackageManager().getPermissionGroupInfo(permissionGroup, 0);
+                            textView.setText(showDescription ? groupInfo.loadDescription(context.get().getPackageManager()) : groupInfo.loadLabel(context.get().getPackageManager()));
+                        } catch (PackageManager.NameNotFoundException e) {
+                            e.printStackTrace();
+                        }
                         permissionsLayout.addView(textView);
                         groupSet.add(permissionGroup);
                     }
--- app/src/main/java/com/app/permissionx/MainActivity.java
+++ app/src/main/java/com/app/permissionx/MainActivity.java
@@ -31,18 +31,23 @@ public class MainActivity extends AppCompatActivity {
                 PermissionX.init(MainActivity.this)
                         .permissions(
                                 Manifest.permission.READ_EXTERNAL_STORAGE,
-                                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.CAMERA,
+                                Manifest.permission.CALL_PHONE)
                         .onExplainRequestReason(new ExplainReasonCallbackWithBeforeParam() {
                             @Override
                             public void onExplainReason(ExplainScope scope, List<String> deniedList, boolean beforeRequest) {
 //                                scope.showRequestReasonDialog(deniedList, "为了保证程序正常工作，请您同意以下权限申请" + deniedList, "我已明白");
-                                scope.showRequestReasonDialog(new CustomDialog(MainActivity.this, "haha", deniedList));
+                                CustomDialog customDialog = new CustomDialog(MainActivity.this, "您拒绝了以下权限", deniedList);
+                                scope.showRequestReasonDialog(customDialog);
                             }
                         })
                         .onForwardToSettings(new ForwardToSettingsCallback() {
                             @Override
                             public void onForwardToSettings(ForwardScope scope, List<String> deniedList) {
-                                scope.showForwardToSettingsDialog(deniedList, "您需要去应用程序设置当中手动开启权限" + deniedList, "我已明白");
+                                CustomDialog customDialog = new CustomDialog(MainActivity.this, "您拒绝了以下权限", deniedList, true);
+                                scope.showForwardToSettingsDialog(customDialog);
+//                                scope.showForwardToSettingsDialog(deniedList, "您需要去应用程序设置当中手动开启权限" + deniedList, "我已明白");
                             }
                         })
                         .request(new RequestCallback() {
